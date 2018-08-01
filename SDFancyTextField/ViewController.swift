1500860f604c7c45f47c5edf879232fae0c89658
John Solsma <solsma@me.com>
2018-07-31 22:05:36 -0500

    Initial Commit

---
+++ SDFancyTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SDFancyTextField
+//
+//  Created by ZuluAlpha on 7/31/18.
+//  Copyright © 2018 Solsma Dev Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
