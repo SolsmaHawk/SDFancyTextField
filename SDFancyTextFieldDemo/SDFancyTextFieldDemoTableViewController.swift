9d443f02c51df8e422de17138a9cdb28c667b331
John Solsma <solsma@me.com>
2018-10-07 12:16:58 -0500

    Started adding notes to sample project

--- SDFancyTextFieldDemo/SDFancyTextFieldDemoTableViewController.swift
+++ SDFancyTextFieldDemo/SDFancyTextFieldDemoTableViewController.swift
@@ -16,12 +16,20 @@ class SDFancyTextFieldDemoTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Most properties can be set during initialization or in Interface Builder
+        
+        // Set email field to allow auto validation *can also be set in IB*
         self.validEmailField.allowAutoValidation = true
+        // Set quick validation type(s) for email field
         self.validEmailField.quickValidationTypes = [.ValidEmail]
+        
+        // Do the same for the first password field
         self.everythingField.allowAutoValidation = true
+        // You can set multiple quick validation types to a field
         self.everythingField.quickValidationTypes = [.ContainsNumber,.SpecialCharacter,.UppercaseLetter]
         self.retypePasswordField.allowAutoValidation = true
         
+        /* Using a field validation closure you can set a completely custom validation type. This field validation closure checks the field to see if contains the character sequence 'test' and also that it has a length of 7 or greater. Field validation closures return a tuple of (Bool, String). The Bool indicates the success of the validation closure and the String is the message displayed within the text field */
         self.everythingField.fieldValidationClosure = {textFieldText in
             if (textFieldText.lowercased().contains("test") && textFieldText.count >= 7) {
                 return (true, nil)
@@ -32,6 +40,7 @@ class SDFancyTextFieldDemoTableViewController: UITableViewController {
             return (false, "Length must be 7 or greater")
         }
         
+        /* A common text form scenario is retyping a password and making sure it matches a previous password field. In the field validation closure below you can see how a second field can be made to match another text field's text. If either text field is changed this text field will be automatically validated to make sure both fields contain the exact same text. */
         self.retypePasswordField.fieldValidationClosure = {textFieldText in
             if textFieldText == self.everythingField.textField.text {
                 return (true,nil)
