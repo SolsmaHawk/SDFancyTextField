0418bc98996baab0783c584e49467937cd4f39c3
John Solsma <solsma@me.com>
2018-10-14 13:20:26 -0500

    Syntax cleanup

--- SDFancyTextFieldDemo/SDFancyTextFieldDemoTableViewController.swift
+++ SDFancyTextFieldDemo/SDFancyTextFieldDemoTableViewController.swift
@@ -24,7 +24,7 @@ class SDFancyTextFieldDemoTableViewController: UITableViewController {
 
         // Most properties can be set during initialization or in Interface Builder
         
-        // Set email field to allow auto validation *can also be set in IB*
+        // Set email field to allow auto validation * can also be set in IB *
         self.validEmailField.allowAutoValidation = true
         // Set quick validation type(s) for email field
         self.validEmailField.quickValidationTypes = [.ValidEmail, .NotEmpty]
@@ -46,8 +46,8 @@ class SDFancyTextFieldDemoTableViewController: UITableViewController {
             return (false, "Length must be 7 or greater")
         }
         
-        
         /* *** You can add your own group validation similar to Quick Validations that can easily be appled to multiple fields. The validation below adds a check for the word 'new' in a text field. In order to apply it to your field, an append statement below adds it in addition to the quick validations already added to the field **
+         
          
          SDFancyTextField.addValidationFor(group: SDFancyTextField.ValidationGroup.init(name: "mustContainNew"), with: {textFieldText in
          if textFieldText.lowercased().contains("new") {
@@ -55,14 +55,16 @@ class SDFancyTextFieldDemoTableViewController: UITableViewController {
          }
          return (false,"Must contain 'new'")
          })
+ 
+         
          // To ADD it to existing validations - self.everythingField.validationGroups?.append(SDFancyTextField.ValidationGroup.init(name: "mustContainNew"))
          // To REPLACE existing validations - self.everythingField.validationGroups = [SDFancyTextField.ValidationGroup.init(name: "mustContainNew")]
          */
         
         /* A common text form scenario is retyping a password and making sure it matches a previous password field. In the field validation closure below you can see how a second field can be made to match another text field's text. If either text field is changed this text field will be automatically validated to make sure both fields contain the exact same text. NOTE: To avoid reference cycles, make sure to reference WEAK self within validation closures */
         self.retypePasswordField.quickValidationTypes = [.NotEmpty]
-        self.retypePasswordField.fieldValidationClosure = { /*Weak reference to self*/ [weak self] textFieldText in
-            if let strongSelf = self { /*Optional binding to check self*/
+        self.retypePasswordField.fieldValidationClosure = { /* Weak reference to self */ [weak self] textFieldText in
+            if let strongSelf = self { /* Optional binding to check self */
                 if textFieldText == strongSelf.everythingField.textField.text {
                     return (true,nil)
                 }
