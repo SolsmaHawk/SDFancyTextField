5bfcfa1ea6726919118ac00aad66125e58c50e58
IMT Mobile Developer <john.solsma@theimtgroup.com>
2018-10-17 11:51:23 -0500

    Access control fixes

--- SDFancyTextField/SDFancyTextField.swift
+++ SDFancyTextField/SDFancyTextField.swift
@@ -826,7 +826,7 @@ public class SDFancyTextField: UIView {
         self.setup()
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 }
